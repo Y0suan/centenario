2f387d15a3bed274882d76738a149cf898173767
DesarolloTecno <roboticaeinovacion@gmail.com>
2024-01-15 10:27:31 -0300

    analiticas por tercera ves

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
 import Layout from '@/Component/Layoud';
 import Loader from '@/Component/Loader';
-import React, { lazy, Suspense } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 const CalendarPage = lazy(() => import('@/Component/Calendar/Calendar'));
 const SectionHome = lazy(() => import('@/Component/Section/SectionHome'));
 const SectionScroll = lazy(() => import('@/Component/Section/SectionScroll'));
 import ReactGA from 'react-ga';
 
-
 const LoaderFallback = () => <Loader/>;
 
 export default function HomePage() {
-  ReactGA.pageview('/');
+  useEffect(() => {
+    // Verifica si ya se inicializó react-ga antes de hacerlo nuevamente
+    if (!window.GA_INITIALIZED) {
+      ReactGA.initialize('G-H0EHX8H1FP'); // Reemplaza con tu ID de seguimiento
+      window.GA_INITIALIZED = true;
+    }
+    ReactGA.pageview('/');
+  }, []);
+
   return (
     <Layout>
       <Suspense fallback={<LoaderFallback />}>
