70ff06493ec2b3eca953069d672dd79809f93a57
DesarolloTecno <roboticaeinovacion@gmail.com>
2024-01-16 06:58:57 -0300

    analiticas de otra manera

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,47 @@
 import Layout from '@/Component/Layoud';
 import Loader from '@/Component/Loader';
 import React, { useEffect, lazy, Suspense } from 'react';
+
 const CalendarPage = lazy(() => import('@/Component/Calendar/Calendar'));
 const SectionHome = lazy(() => import('@/Component/Section/SectionHome'));
 const SectionScroll = lazy(() => import('@/Component/Section/SectionScroll'));
-import ReactGA from 'react-ga';
 
-const LoaderFallback = () => <Loader/>;
+const LoaderFallback = () => <Loader />;
 
 export default function HomePage() {
   useEffect(() => {
-    // Verifica si ya se inicializó react-ga antes de hacerlo nuevamente
     if (!window.GA_INITIALIZED) {
-      ReactGA.initialize('G-H0EHX8H1FP'); // Reemplaza con tu ID de seguimiento
+      initializeGA('G-H0EHX8H1FP'); // Reemplaza con tu ID de seguimiento
       window.GA_INITIALIZED = true;
     }
-    ReactGA.pageview('/');
+    logPageView();
   }, []);
-
+  
+  const initializeGA = (measurementId) => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', measurementId);
+    window.gtag = gtag; // Asigna la función gtag a window.gtag
+  };
+  
+  const logPageView = () => {
+    if (window.GA_INITIALIZED) {
+      window.gtag('config', 'G-H0EHX8H1FP', { 'page_path': window.location.pathname });
+    }
+  };
   return (
-    <Layout>
-      <Suspense fallback={<LoaderFallback />}>
-        <SectionHome />
-        <CalendarPage />
-        <SectionScroll />
-      </Suspense>
-    </Layout>
+    <>
+      {/* Contenido del componente */}
+      <Layout>
+        <Suspense fallback={<LoaderFallback />}>
+          <SectionHome />
+          <CalendarPage />
+          <SectionScroll />
+        </Suspense>
+      </Layout>
+    </>
   );
 }
--- public/gtag.js
+++ public/gtag.js
@@ -1,21 +1,18 @@
-
 export const GA_MEASUREMENT_ID = 'G-H0EHX8H1FP'; // Reemplaza con tu ID de seguimiento
 
+export const GA_DEBUG_MODE = true; // Puedes ajustar esto según tus necesidades
+
+export const GA_TRACKING_ID = `UA-${GA_MEASUREMENT_ID}-1`;
+
+// Inicializar gtag.js
 export const initGA = () => {
   if (!window.GA_INITIALIZED) {
     window.dataLayer = window.dataLayer || [];
     function gtag() {
       window.dataLayer.push(arguments);
     }
-    window.gtag = gtag; // Asigna la función gtag a window.gtag
     gtag('js', new Date());
-    gtag('config', GA_MEASUREMENT_ID);
+    gtag('config', GA_TRACKING_ID, { 'debug_mode': GA_DEBUG_MODE });
     window.GA_INITIALIZED = true;
-  }
-};
-
-export const logPageView = () => {
-  if (window.GA_INITIALIZED) {
-    window.gtag('config', GA_MEASUREMENT_ID, { 'page_path': window.location.pathname });
   }
 };
