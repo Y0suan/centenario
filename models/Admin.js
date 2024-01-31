4d28cfb52e5f83d582c8d2131d256cf19f78aabf
DesarolloTecno <roboticaeinovacion@gmail.com>
2024-01-31 11:37:31 -0300

    mejoras

--- models/Admin.js
+++
@@ -1,7 +0,0 @@
-import {model, models, Schema} from "mongoose";
-
-const adminSchema = new Schema({
-  email: {type: String, required: true, unique: true},
-}, {timestamps: true});
-
-export const Admin = models?.Admin || model('Admin', adminSchema);
